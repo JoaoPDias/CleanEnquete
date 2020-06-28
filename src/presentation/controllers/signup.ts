08ad91258cdf77777ae200391c8d8730fd70a88a
Joao Paulo Dias <jpdias1997@hotmail.com>
2020-06-27 22:56:41 -0300

    feat: ensure SignUpController returns 500 if EmailValidator throws a generic error

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,9 +1,10 @@
 import {HttpRequest, HttpResponse} from "../protocols/http";
 import {MissingParamError} from "../errors/missing-param-error";
-import {badRequest} from "../helpers/http-helper";
+import {badRequest, serverError} from "../helpers/http-helper";
 import {Controller} from "../protocols/controller";
 import {EmailValidator} from "../protocols/email-validator";
-import {InvalidParamError} from "../errors/invalidParamError";
+import {InvalidParamError} from "../errors/invalid-param-error";
+import {ServerError} from "../errors/server-error";
 
 export class SignUpController implements Controller {
     private readonly _emailValidator: EmailValidator;
@@ -14,15 +15,19 @@ export class SignUpController implements Controller {
     }
 
     handle(httpRequest: HttpRequest): HttpResponse {
-        const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
-        for (const field of requiredFields) {
-            if (!httpRequest.body[field]) {
-                return badRequest(new MissingParamError(field))
+        try {
+            const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
+            for (const field of requiredFields) {
+                if (!httpRequest.body[field]) {
+                    return badRequest(new MissingParamError(field))
+                }
             }
-        }
-        const isValid = this._emailValidator.isValid(httpRequest.body.email)
-        if (!isValid) {
-            return badRequest(new InvalidParamError('email'))
+            const isValid = this._emailValidator.isValid(httpRequest.body.email)
+            if (!isValid) {
+                return badRequest(new InvalidParamError('email'))
+            }
+        } catch (e) {
+            return serverError(new ServerError())
         }
     }
 }
