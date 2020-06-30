e5c4259a42674ad745fa1a1b02afa5337646134c
Joao Paulo Dias <jpdias1997@hotmail.com>
2020-06-30 01:37:06 -0300

    test: ensure AccountMongoRepository returns an account on success

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -1,4 +1,5 @@
 import {MongoHelper} from "../helpers/mongo-helper";
+import {AccountMongoRepository} from "./accountMongoRepository";
 
 describe('Account Mongo Repository', () => {
 
@@ -8,5 +9,19 @@ describe('Account Mongo Repository', () => {
 
     afterAll(async () => {
         await MongoHelper.disconnect()
+    })
+
+    test('Should return an account on succss', async () => {
+        const sut = new AccountMongoRepository()
+        const account = await sut.add({
+            name: 'Any Name',
+            email: 'any_email@mail.com',
+            password: 'any_password'
+        })
+        expect(account).toBeTruthy()
+        expect(account.id).toBeTruthy()
+        expect(account.name).toBe('Any Name')
+        expect(account.email).toBe('any_email@mail.com')
+        expect(account.password).toBe('any_password')
     })
 })
