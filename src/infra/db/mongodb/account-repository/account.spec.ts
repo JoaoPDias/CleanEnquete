9d97670b6d4273f0854ded0dff52e7671c7ba988
Joao Paulo Dias <jpdias1997@hotmail.com>
2020-06-30 01:39:55 -0300

    refactor: clear database before each test

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -10,9 +10,15 @@ describe('Account Mongo Repository', () => {
     afterAll(async () => {
         await MongoHelper.disconnect()
     })
-
+    beforeEach(async () => {
+        const accountCollection = MongoHelper.getCollection('accounts')
+        await accountCollection.deleteMany({})
+    })
+    const makeSut = () : AccountMongoRepository => {
+        return new AccountMongoRepository()
+    }
     test('Should return an account on succss', async () => {
-        const sut = new AccountMongoRepository()
+        const sut = makeSut()
         const account = await sut.add({
             name: 'Any Name',
             email: 'any_email@mail.com',
