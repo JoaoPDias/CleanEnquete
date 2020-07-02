ae4c4f685a732111277df54a41d195444b827252
Joao Paulo Dias <jpdias1997@hotmail.com>
2020-07-02 01:28:13 -0300

    feat: ensure LogControllerDecorator calls controller with correct value

---
+++ src/main/decorators/log.ts
@@ -0,0 +1,18 @@
+import {Controller, HttpRequest, HttpResponse} from "../../presentation/protocols";
+
+export class LogControllerDecorator implements Controller {
+    private readonly _controller : Controller;
+
+    constructor(controller : Controller) {
+        this._controller = controller;
+    }
+
+    async handle(httpRequest : HttpRequest) : Promise<HttpResponse> {
+        const httpResponse = await this._controller.handle(httpRequest)
+        if (httpResponse.statusCode === 500) {
+        }
+        return httpResponse
+
+    }
+
+}
--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -3,12 +3,16 @@ import {EmailValidatorAdapter} from "../../utils/email-validator-adapter";
 import {DbAddAccount} from "../../data/usecases/add-account/db-add-account";
 import {BCryptAdapter} from "../../infra/criptography/bcrypt-adapter";
 import {AccountMongoRepository} from "../../infra/db/mongodb/account-repository/account";
+import {Controller} from "../../presentation/protocols";
+import {LogControllerDecorator} from "../decorators/log";
 
-export const makeSignUpController = () : SignUpController => {
+
+export const makeSignUpController = () : Controller => {
     const emailValidatorAdapter = new EmailValidatorAdapter()
     const salt = 12
     const bCryptAdapter = new BCryptAdapter(salt);
-    const addAccountRepostitory = new AccountMongoRepository()
-    const dbAddAccount = new DbAddAccount(bCryptAdapter, addAccountRepostitory)
-    return new SignUpController(emailValidatorAdapter, dbAddAccount)
+    const addAccountRepository = new AccountMongoRepository()
+    const dbAddAccount = new DbAddAccount(bCryptAdapter, addAccountRepository)
+    let signUpController = new SignUpController(emailValidatorAdapter, dbAddAccount)
+    return new LogControllerDecorator(signUpController)
 }
