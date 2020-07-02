7dcbc528fd52ee2a0bbb4dfde4ffec8de3ab05d5
Joao Paulo Dias <jpdias1997@hotmail.com>
2020-07-02 00:29:14 -0300

    feat: ensure MongoDB reconnects if the connection is lost

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -2,7 +2,7 @@ import {SignUpController} from "../../presentation/controllers/signup/signup";
 import {EmailValidatorAdapter} from "../../utils/email-validator-adapter";
 import {DbAddAccount} from "../../data/usecases/add-account/db-add-account";
 import {BCryptAdapter} from "../../infra/criptography/bcrypt-adapter";
-import {AccountMongoRepository} from "../../infra/db/mongodb/account-repository/accountMongoRepository";
+import {AccountMongoRepository} from "../../infra/db/mongodb/account-repository/account";
 
 export const makeSignUpController = () : SignUpController => {
     const emailValidatorAdapter = new EmailValidatorAdapter()
