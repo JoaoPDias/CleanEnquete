55d460756d7ab454d3a07c86a94ce553a55705ad
Joao Paulo Dias <jpdias1997@hotmail.com>
2020-07-07 23:54:54 -0300

    test: ensure Validation Composite returns the first error if more than one validation fails

--- src/presentation/helpers/validators/validation-composite.spec.ts
+++ src/presentation/helpers/validators/validation-composite.spec.ts
@@ -1,5 +1,5 @@
 import {ValidationComposite} from "./validation-composite";
-import {MissingParamError} from "../../errors";
+import {InvalidParamError, MissingParamError} from "../../errors";
 import {Validation} from "./validation";
 
 const makeValidationStub = () : Validation => {
@@ -15,19 +15,26 @@ const makeValidationStub = () : Validation => {
 
 interface SutTypes {
     sut : ValidationComposite
-    validationStub : Validation
+    validationStubs : Validation[]
 }
 
 const makeSut = () : SutTypes => {
-    const validationStub = makeValidationStub()
-    const sut = new ValidationComposite([validationStub])
-    return {sut, validationStub}
+    const validationStubs = [makeValidationStub(), makeValidationStub()]
+    const sut = new ValidationComposite(validationStubs)
+    return {sut, validationStubs}
 }
 describe('Validation Composite', () => {
     test('Should return an error if any validation fails', () => {
-        const {sut, validationStub} = makeSut()
-        jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new MissingParamError('name'))
+        const {sut, validationStubs} = makeSut()
+        jest.spyOn(validationStubs[0], 'validate').mockReturnValueOnce(new MissingParamError('name'))
         const error = sut.validate({field: 'any_value'})
         expect(error).toEqual(new MissingParamError('name'))
+    });
+    test('Should return first error if more than one validation fails', () => {
+        const {sut, validationStubs} = makeSut()
+        jest.spyOn(validationStubs[0], 'validate').mockReturnValueOnce(new InvalidParamError('email'))
+        jest.spyOn(validationStubs[1], 'validate').mockReturnValueOnce(new MissingParamError('password'))
+        const error = sut.validate({field: 'any_value'})
+        expect(error).toEqual(new InvalidParamError('email'))
     });
 });
