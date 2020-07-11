e6a4267f54e41b158c3b780015c1492360ecf328
Joao Paulo Dias <jpdias1997@hotmail.com>
2020-07-11 15:52:59 -0300

    refactor: move validation to a folder Protocols

--- src/presentation/controllers/signup/signup-protocols.ts
+++ src/presentation/controllers/signup/signup-protocols.ts
@@ -1,6 +1,6 @@
 export * from '../../protocols'
 export * from '../../protocols/email-validator'
-export * from '../../helpers/validators/validation'
+export * from '../../protocols/validation'
 export * from '../../../domain/usecases/add-account'
 export * from '../../../domain/models/account'
 export * from "../../../domain/builders/account-model-builder";
