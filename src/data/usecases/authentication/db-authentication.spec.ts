de9de4bd4baf7f5ad7476f3deab072782de494ed
Joao Paulo Dias <jpdias1997@hotmail.com>
2020-07-11 20:43:20 -0300

    test: ensure DbAuthentication calls HashComparer with correct values

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -1,34 +1,48 @@
-import {AccountModel} from "../../../domain/models/account";
-import {AccountModelBuilder} from "../../../domain/builders/account-model-builder";
-import {LoadAccountByEmailRepository} from "../../protocols/db/load-account-by-email.repository";
 import {DbAuthentication} from "./db.authentication";
 import {AuthenticationModelBuilder} from "../../../presentation/builders/authentication-model-builder";
+import {LoadAccountByEmailRepository} from "../../protocols/db/load-account-by-email-repository";
+import {AccountModelBuilder} from "../../../domain/builders/account-model-builder";
+import {AccountModel} from "../../../domain/models/account";
+import {HashComparer} from "../../protocols/criptography/hash-comparer";
 
+const authenticationModel = AuthenticationModelBuilder.new().build()
+const accountModel = AccountModelBuilder.new().password('hashed_password').build()
 const makeLoadAccountByEmail = () => {
     class LoadAccountByEmailRepositoryStub implements LoadAccountByEmailRepository {
         async load(email : string) : Promise<AccountModel> {
-            const account = AccountModelBuilder.new().build()
-            return Promise.resolve(account)
+            return Promise.resolve(accountModel)
         }
     }
 
     return new LoadAccountByEmailRepositoryStub()
 }
 
+const makeHashComparer = () => {
+    class HashComparerStub implements HashComparer {
+        async compare(value : string, hash : string) : Promise<boolean> {
+            return Promise.resolve(true)
+        }
+    }
+
+    return new HashComparerStub()
+}
 interface SutTypes {
     sut : DbAuthentication
     loadAccountByEmailRepositoryStub : LoadAccountByEmailRepository
+    hashComparerStub : HashComparer
 }
+
 
 const makeSut = () : SutTypes => {
     const loadAccountByEmailRepositoryStub = makeLoadAccountByEmail()
-    const sut = new DbAuthentication(loadAccountByEmailRepositoryStub)
+    const hashComparerStub = makeHashComparer()
+    const sut = new DbAuthentication(loadAccountByEmailRepositoryStub, hashComparerStub)
     return {
         sut,
-        loadAccountByEmailRepositoryStub
+        loadAccountByEmailRepositoryStub,
+        hashComparerStub
     }
 }
-const authenticationModel = AuthenticationModelBuilder.new().build()
 describe('DbAuthentication UseCase', () => {
     test('Should call LoadAccountByEmailRepository with correct email', async () => {
         const {sut, loadAccountByEmailRepositoryStub} = makeSut()
@@ -47,5 +61,11 @@ describe('DbAuthentication UseCase', () => {
         jest.spyOn(loadAccountByEmailRepositoryStub, 'load').mockReturnValueOnce(null)
         const accessToken = await sut.auth(authenticationModel)
         expect(accessToken).toBeNull()
+    });
+    test('Should call HashComparer with correct password', async () => {
+        const {sut, hashComparerStub} = makeSut()
+        const compareSpy = jest.spyOn(hashComparerStub, 'compare')
+        await sut.auth(authenticationModel)
+        expect(compareSpy).toHaveBeenCalledWith(authenticationModel.password, accountModel.password)
     });
 });
