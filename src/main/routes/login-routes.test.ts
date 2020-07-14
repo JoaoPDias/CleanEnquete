f8bcb079402661baea6548942ef54508380385fc
Joao Paulo Dias <jpdias1997@hotmail.com>
2020-07-14 00:12:02 -0300

    refactor: rename signup routes with login routes

---
+++ src/main/routes/login-routes.test.ts
@@ -0,0 +1,33 @@
+import app from "../config/app";
+import request from "supertest";
+import {MongoHelper} from "../../infra/db/mongodb/helpers/mongo-helper";
+
+describe('Login Routes', () => {
+
+
+    beforeAll(async () => {
+        await MongoHelper.connect(process.env.MONGO_URL)
+    })
+
+    afterAll(async () => {
+        await MongoHelper.disconnect()
+    })
+    beforeEach(async () => {
+        const accountCollection = await MongoHelper.getCollection('accounts')
+        await accountCollection.deleteMany({})
+    })
+    describe('POST /signup', () => {
+        test('Should 200 on signup', async () => {
+            await request(app)
+                .post('/api/signup')
+                .send({
+                    name: 'João Paulo Dias',
+                    email: 'diasjp1997@gmail.com',
+                    password: '123',
+                    passwordConfirmation: '123'
+                })
+                .expect(200)
+
+        })
+    })
+});
