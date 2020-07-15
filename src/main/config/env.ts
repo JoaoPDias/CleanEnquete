ab4dba655e61367234ade8b4fc11320aa7696fd6
Joao Paulo Dias <jpdias1997@hotmail.com>
2020-07-15 03:06:31 -0300

    refactor: change routes to use fs instead fast-glob

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -3,8 +3,8 @@ import dotenv from 'dotenv'
 dotenv.config();
 let Environment
 if (process.env.NODE_ENV === 'test') {
-    Environment = require(`./env/development.ts`);
+    Environment = require(`./env/development`);
 } else {
-    Environment = require(`./env/${process.env.NODE_ENV}.ts`);
+    Environment = require(`./env/${process.env.NODE_ENV}`);
 }
 export default Environment.Environment
