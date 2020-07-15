821851dd33777b9ee6f1116ca5be7617cd0a7684
Joao Paulo Dias <jpdias1997@hotmail.com>
2020-07-15 01:25:40 -0300

    test: add login route

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -1,7 +1,9 @@
 import app from "../config/app";
 import request from "supertest";
 import {MongoHelper} from "../../infra/db/mongodb/helpers/mongo-helper";
+import {hash} from "bcrypt";
 
+let accountCollection
 describe('Login Routes', () => {
 
 
@@ -13,7 +15,7 @@ describe('Login Routes', () => {
         await MongoHelper.disconnect()
     })
     beforeEach(async () => {
-        const accountCollection = await MongoHelper.getCollection('accounts')
+        accountCollection = await MongoHelper.getCollection('accounts')
         await accountCollection.deleteMany({})
     })
     describe('POST /signup', () => {
@@ -29,5 +31,22 @@ describe('Login Routes', () => {
                 .expect(200)
 
         })
+    })
+    describe('POST /login', () => {
+        test('Should return 200 on login', async () => {
+            const password = await hash('123', 12)
+            await accountCollection.insertOne({
+                name: 'Rodrigo',
+                email: 'rodrigo.manguinho@gmail.com',
+                password
+            })
+            await request(app)
+                .post('/api/login')
+                .send({
+                    email: 'rodrigo.manguinho@gmail.com',
+                    password: '123'
+                })
+                .expect(200)
+        });
     })
 });
