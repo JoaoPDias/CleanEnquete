e189f169400dd12f2dd1b38295a56debdcc063a6
Joao Paulo Dias <jpdias1997@hotmail.com>
2020-07-16 23:59:01 -0300

    feat: ensure SignUpController returns 403 if AddAccount returns null

--- src/presentation/controllers/signup/signup-controller.ts
+++ src/presentation/controllers/signup/signup-controller.ts
@@ -6,7 +6,8 @@ import {
     HttpResponse,
     Validation
 } from "./signup-controller-protocols";
-import {badRequest, serverError, success} from "../../helpers/http/http-helper";
+import {badRequest, forbidden, serverError, success} from "../../helpers/http/http-helper";
+import {EmailInUseError} from "../../errors";
 
 export class SignUpController implements Controller {
 
@@ -20,11 +21,14 @@ export class SignUpController implements Controller {
                 return badRequest(error)
             }
             const {name, email, password} = httpRequest.body
-            await this._addAccount.add({
+            const account = await this._addAccount.add({
                 name,
                 email,
                 password
             })
+            if (!account) {
+                return forbidden(new EmailInUseError(email))
+            }
             const accessToken = await this._authentication.auth({email, password})
             return success({accessToken})
         } catch (e) {
--- src/presentation/errors/invalid-param-error.ts
+++ src/presentation/errors/invalid-param-error.ts
@@ -1,7 +1,15 @@
 export class InvalidParamError extends Error {
-    constructor(paramName: string) {
+    constructor(paramName : string) {
         super(`Invalid param: ${paramName}`)
         this.name = 'InvalidParamError'
+    }
+
+}
+
+export class EmailInUseError extends Error {
+    constructor(email : string) {
+        super(`The email "${email}" is already in use: ${email}`)
+        this.name = 'EmailInUseError'
     }
 
 }
