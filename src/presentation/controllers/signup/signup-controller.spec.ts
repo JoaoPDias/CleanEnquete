3952858908ece4912cfbbf0dc8e440b8ca1a3909
Joao Paulo Dias <jpdias1997@hotmail.com>
2020-07-16 23:39:07 -0300

    test: ensure SignUpController returns 200 if Auth Authentication return an acessToken on success

--- src/presentation/controllers/signup/signup-controller.spec.ts
+++ src/presentation/controllers/signup/signup-controller.spec.ts
@@ -115,4 +115,9 @@ describe('SignUp Controller', () => {
         const httpResponse = await sut.handle(httpRequestDefault)
         expect(httpResponse).toStrictEqual(serverError(new Error()))
     });
+    test('Should return 200 if valid credentials are provided', async () => {
+        const {sut} = makeSut()
+        const httpResponse = await sut.handle(httpRequestDefault)
+        expect(httpResponse).toStrictEqual(success({accessToken: 'valid_token'}))
+    });
 })
